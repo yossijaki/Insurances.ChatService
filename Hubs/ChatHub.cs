6d14ad4ccc4461b038bee800040496344eb9640a
Yossijaki <yossijaki@gmail.com>
2024-06-03 16:48:10 -0600

    ...

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Insurance.ChatService.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Insurances.ChatService.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task JoinChat(UserConnection conn)
+    {
+        await Clients.All
+            .SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined");
+    }
+
+    public async Task JoinSpecificChatRoom(UserConnection conn)
+    {
+        await Groups.AddToGroupAsync(Context.CinnectionId, conn.ChatRoom);
+    }
+}
---
+++ Models/UserConnection.cs
@@ -0,0 +1,6 @@
+namespace Insurance.ChatService.Models;
+
+public class UserConnection {
+    public string Username { get; set; } = string.Empty;
+    public string ChatRoom { get; set; } = string.Empty;
+}
