64375c4d1176ec73dc25704dd32d13275295811b
Yossijaki <yossijaki@gmail.com>
2024-06-04 17:30:39 -0600

    Specific chat room joint function

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -14,5 +14,6 @@ public class ChatHub : Hub
     public async Task JoinSpecificChatRoom(UserConnection conn)
     {
         await Groups.AddToGroupAsync(Context.CinnectionId, conn.ChatRoom);
+        await Clients.Group(conn.ChatRoom).SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined {conn.ChatRoom}");
     }
 }
